b0e01bc6f6f31b7d672dac15454b1bd1f073823d
Kayla Park <kaylapark@Kaylas-MBP.fios-router.home>
2021-10-13 13:46:29 -0400

    working on drag

--- scripts2.js
+++ scripts2.js
@@ -32,3 +32,80 @@ people.forEach(p => {
         circles[idx_hide].classList.toggle("hide");
     });
 });
+
+const slider = document.querySelector('.items');
+const grid = document.querySelector("main");
+//CIRCLE CURSOR W FOLLOWING DOT
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+var hover = false;
+var body = document.querySelector("body");
+
+if(hover == false) {
+    slider.addEventListener('mouseover', (e) => {
+        $bigBall.classList.toggle("show");
+        hover = true;
+        body.style.cursor = "none";
+    });
+}
+
+$bigBall.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  $bigBall.classList.toggle("show");
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+  hover = false;
+});
+
+$bigBall.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+
+
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+  TweenMax.to($bigBall, .4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15
+  })
+}
+
+// Hover an element
+function onMouseHover() {
+  TweenMax.to($bigBall, .3, {
+    scale: 4
+  })
+}
+function onMouseHoverOut() {
+  TweenMax.to($bigBall, .3, {
+    scale: 1
+  })
+}
