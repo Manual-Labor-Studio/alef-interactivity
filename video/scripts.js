7fb9aabc83ef0ded67d540f8efceed5e8c87769a
Kayla Park <kaylapark@Kaylas-MBP.fios-router.home>
2021-10-20 12:46:01 -0400

    video

---
+++ video/scripts.js
@@ -0,0 +1,55 @@
+const play = document.querySelector('.play');
+const circle_div = document.querySelector('.circle');
+const video = document.querySelector('video');
+const close = document.querySelector('.close');
+
+var full_screen = false;
+
+
+play.addEventListener('mouseover', function() {
+    if(full_screen == false) {
+        video.classList.remove("circle_hide");
+        video.play();
+    }
+});
+
+play.addEventListener('mouseout', function(){
+    if(full_screen == false) {
+        video.classList.add("circle_hide");
+    }
+});
+
+play.addEventListener('click', function(){
+    if(full_screen == false) {
+        full_screen = true;
+        video.pause();
+        video.currentTime = 0;
+        video.controls = true;
+        video.classList.remove('circle_video');
+        video.classList.add('full_video');
+        close.classList.toggle('toggle_close');
+        setTimeout(function(){
+            video.play();
+        }, 2000);
+    }
+});
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape" && full_screen == true) {
+        close_video();
+	}
+});
+
+close.addEventListener('click', function() {
+    if(full_screen == true) {
+        close_video();
+    }
+});
+
+function close_video(){
+    close.classList.toggle('toggle_close');
+    video.classList.remove('full_video');
+    video.classList.add('circle_video');
+    video.classList.add('circle_hide');
+    full_screen = false;
+}
